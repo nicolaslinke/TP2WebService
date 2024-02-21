d06d73493a73b86a353fc204deb30af014e03d91
n <2185802@cegepmontpetit.ca>
2024-02-21 10:20:08 -0500

    Importation de code html

--- tp2/src/app/app.module.ts
+++ tp2/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
+import { ShowComponent } from './show/show.component';
+import { SongComponent } from './song/song.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      ArtistComponent,
+      AlbumComponent,
+      ShowComponent,
+      SongComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
